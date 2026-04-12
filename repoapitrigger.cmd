9c6d9a5e9933b0a6032b6225b202f12b73a42f33
Your Name <you@example.com>
2026-04-12 19:28:27 +0530

    repotrigger

--- repoapitrigger.cmd
+++ repoapitrigger.cmd
@@ -1,5 +1,5 @@
 curl -X POST ^
   -H "Accept: application/vnd.github+json" ^
   -H "Authorization: Bearer %GITHUB_PAT%" ^
-  https://api.github.com/repos/OWNER/REPO/dispatches ^
+  https://api.github.com/repos/sreeharinath1/github_actions/dispatches ^
   -d "{ \"event_type\": \"deploy\", \"client_payload\": { \"environment\": \"production\", \"version\": \"v1.2.3\", \"triggered-by\": \"jenkins\" } }"
